a6e8eae2558ddb166ed79dea60d4a93e0f5a84c9
Joe Crypto <joecrypto300@gmail.com>
2022-01-22 17:56:55 +0000

    run db migrations

--- import_snapshot.sh
+++ import_snapshot.sh
@@ -2,5 +2,7 @@
 
 #docker run --network etl_etl -e "DATABASE_URL=postgresql://etl:password@postgres:5432/helium_blockchain" helium/etl:1.1.135 migrations setup
 
-docker exec postgres-etl pg_restore -v -d helium_blockchain -W -j4 -U etl -Fd /var/data/root/export/database
+docker run --network etl_etl -e "DATABASE_URL=postgresql://etl:password@postgres:5432/helium_blockchain" helium/etl:1.1.135 migrations run
+
+docker exec -it postgres-etl pg_restore -v -d helium_blockchain -W -j32 -U etl -Fd /var/data/root/export/database
 
